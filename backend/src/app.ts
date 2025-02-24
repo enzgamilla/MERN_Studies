fbdef6c1a22d8f35849f74527b4dcd8fc5b92843
enzgamilla <laurenceangelogamilla@gmail.com>
2025-02-24 16:24:03 +0800

    create a dummydata and interface for data

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,17 +5,9 @@ import UserRouter from "./routes/users-routes";
 const app = express();
 
 app.use(express.json());
+
 app.use(PlaceRouter);
 app.use(UserRouter);
-
-// Basic Server setup
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  try {
-    res.send("Hello World!");
-  } catch (error) {
-    res.status(401).json({ error: "Something went wrong!" });
-  }
-});
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
---
+++ backend/src/shared/types.ts
@@ -0,0 +1,10 @@
+export interface Place {
+  id: string;
+  image: string;
+  title: string;
+  description: string;
+  address: string;
+  creatorID: string;
+  locationUrl: string;
+  dateCreated: string;
+}
